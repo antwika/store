76ab928e431fa97649d320e3ff71629ad4bde02e
Anton Wiklund <ixuz07@gmail.com>
2022-07-12 18:01:37 +0200

    fix: add missing MongoDbConnection type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './IStore';
 export * from './IPartitionStore';
+export * from './MongoDbConnection';
 export * from './MongoDbStore';
 export * from './MongoDbPartitionStore';
 export * from './MemoryStore';
